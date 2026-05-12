6cb92764b58098eef3280d923a8133962fac63bc
shwballl <kyzyanazar@gmail.com>
2026-05-12 15:48:50 +0300

    Adding some project quality tests

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const { Pool } = require("pg");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "public")));
-const cors = require("cors");
 app.use(cors());
+
+// Додаємо maxAge: '1d' для кешування статичних файлів на 1 день
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    maxAge: "1d",
+  }),
+);
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: { rejectUnauthorized: false },
 });
 
 app.get("/api/movies", async (req, res) => {
