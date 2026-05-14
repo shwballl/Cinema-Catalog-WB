ea5f303a07dbbc8ffc627bca9a266dc91925c526
shwballl <kyzyanazar@gmail.com>
2026-05-14 12:32:58 +0300

    Fix

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const { Pool } = require("pg");
 const path = require("path");
-const cors = require("cors");
 
 const app = express();
 app.use(express.json());
